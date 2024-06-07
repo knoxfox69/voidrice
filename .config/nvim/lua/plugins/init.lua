95aa859aceb663c5009227e1acfe7f139b069ed9
windowsagent <juanjphc@gmail.com>
2024-06-06 23:13:56 -0600

    New nvim config

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,76 @@
+local plugins = {
+  { lazy = true, "nvim-lua/plenary.nvim" },
+
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+    config = true,
+  },
+
+  -- file tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+
+  -- icons, for UI related plugins
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup()
+    end,
+  },
+
+  -- syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require "plugins.configs.treesitter"
+    end,
+  },
+
+  -- statusline
+
+  {
+    "echasnovski/mini.statusline",
+    config = function()
+      require("mini.statusline").setup { set_vim_settings = false }
+    end,
+  },
+
+  -- indent lines
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("ibl").setup {
+        indent = { char = "│" },
+        scope = { char = "│", highlight = "Comment" },
+      }
+    end,
+  },
+
+  -- files finder etc
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    config = function()
+      require "plugins.configs.telescope"
+    end,
+  },
+
+  -- git status on signcolumn etc
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+}
+
+require("lazy").setup(plugins, require "lazy_config")
