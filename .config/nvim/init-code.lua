9efdde01f99097057575f2ca58ca9ae3dc69607b
windowsagent <juanjphc@gmail.com>
2024-07-21 11:46:47 -0600

    Add init lua for vscode

---
+++ .config/nvim/init-code.lua
@@ -0,0 +1,30 @@
+require "options"
+require "mappings"
+require "commands"
+
+-- bootstrap plugins & lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim" -- path where its going to be installed
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+    config = true,
+  }
+}
+require("lazy").setup(plugins, require "lazy_config")
+
+vim.cmd "colorscheme carbonfox"
