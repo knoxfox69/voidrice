5ea02777970fab157d8e4d1833e5831aff17524b
Juan Pablo Hernandez Corcuera <juanjphc@gmail.com>
2026-03-06 11:06:13 -0600

    Switch to hyprlock

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,1 +1,10 @@
-pidof swaylock || swaylock --image ~/.config/hypr/Wallpapers/Train.jpg --clock --indicator --line-color cdd6f4ff --text-color cdd6f4ff --inside-color 1e1e2eff --ring-color 313244ff --line-ver-color cdd6f4ff --text-ver-color cdd6f4ff --inside-ver-color 1e1e2eff --ring-ver-color 313244ff --line-clear-color cdd6f4ff --text-clear-color cdd6f4ff --inside-clear-color 1e1e2eff --ring-clear-color 313244ff --line-clear-color cdd6f4ff --text-wrong-color 313244ff --inside-wrong-color f38ba8ff --ring-wrong-color 313244ff --key-hl-color cba6f7ff
+#!/bin/bash
+
+# Kill any existing lock screen processes
+pkill -9 hyprlock 2>/dev/null
+
+# Small delay to ensure processes are killed
+sleep 0.5
+
+# Launch hyprlock
+hyprlock
